03da7e70b1bed26da387ff30c4c67a457ecfd1f0
mangri <112893833+mangri@users.noreply.github.com>
2022-09-15 23:53:15 +0200

    Create spacesInString.cs

---
+++ spacesInString.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace SpacesInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = askingForInput("Compose and enter any sentence");
+            Console.WriteLine("Your sentence had {0} space(s)", numberOfSpaces(userInput));
+            Console.ReadLine();
+        }
+        static string askingForInput(string ask)
+        {
+            Console.WriteLine(ask);
+            return Console.ReadLine();
+        }
+        static int numberOfSpaces(string userInput)
+        {
+            int spaces = 0;
+            foreach (char s in userInput)
+            {
+                if (s == ' ') spaces++;
+            }
+            return spaces;
+        }
+    }
+}
